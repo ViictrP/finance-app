020490e513e86e74e8167521c5eb4587847940b1
Victor Prado <victor.prado@arc-one.com>
2025-04-03 01:38:04 -0300

    fixing the build

--- finance-app-backend/src/main/java/com/victor/financeapp/backend/Application.java
+++ finance-app-backend/src/main/java/com/victor/financeapp/backend/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+//FIX
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class Application {
