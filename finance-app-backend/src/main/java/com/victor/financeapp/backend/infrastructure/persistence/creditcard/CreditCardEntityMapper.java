fe1fff04e5b9e45ede45506324781672ee50ef21
Victor Prado <victor.prado@arc-one.com>
2025-04-12 03:21:19 -0300

    fixing the build failure

--- finance-app-backend/src/main/java/com/victor/financeapp/backend/infrastructure/persistence/creditcard/CreditCardEntityMapper.java
+++ finance-app-backend/src/main/java/com/victor/financeapp/backend/infrastructure/persistence/creditcard/CreditCardEntityMapper.java
@@ -1,10 +1,11 @@
 package com.victor.financeapp.backend.infrastructure.persistence.creditcard;
 
 import com.victor.financeapp.backend.domain.model.creditcard.CreditCard;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 interface CreditCardEntityMapper {
 
     @Mapping(target = "color", source = "backgroundColor")
