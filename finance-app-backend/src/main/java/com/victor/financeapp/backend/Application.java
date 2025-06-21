062d4961b936d6d7ada885245c48f3e3aab78152
Victor Prado <victor.prado@arc-one.com>
2025-06-21 20:00:46 -0300

    enabling scheduling

--- finance-app-backend/src/main/java/com/victor/financeapp/backend/Application.java
+++ finance-app-backend/src/main/java/com/victor/financeapp/backend/Application.java
@@ -3,10 +3,11 @@ package com.victor.financeapp.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-//FIX
 @SpringBootApplication
 @EnableR2dbcRepositories
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
